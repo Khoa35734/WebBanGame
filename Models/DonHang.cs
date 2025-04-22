02ccc6e618aaa5894880627440ef3d4d33eeb6f5
Khoa Pham <102230247@sv1.dut.udn.vn>
2025-04-22 20:12:24 +0700

    dsajdasdsa

---
+++ Models/DonHang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanGame.Models;
+
+public partial class DonHang
+{
+    public int MaDh { get; set; }
+
+    public int MaKh { get; set; }
+
+    public DateOnly NgayTao { get; set; }
+
+    public bool TrangThaiHuyDon { get; set; }
+
+    public bool ThanhToan { get; set; }
+
+    public DateOnly NgayThanhToan { get; set; }
+
+    public string? Note { get; set; }
+
+    public virtual ICollection<ChiTietDonHang> ChiTietDonHangs { get; set; } = new List<ChiTietDonHang>();
+
+    public virtual KhachHang MaKhNavigation { get; set; } = null!;
+}
---
+++ Models/KhachHang.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanGame.Models;
+
+public partial class KhachHang
+{
+    public int MaKh { get; set; }
+
+    public string TenKh { get; set; } = null!;
+
+    public string Diachi { get; set; } = null!;
+
+    public DateOnly Ngaysinh { get; set; }
+
+    public int Phone { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public DateOnly CreateDate { get; set; }
+
+    public string? TenTaiKhoan { get; set; }
+
+    public string? MatKhau { get; set; }
+
+    public decimal? SoDuTk { get; set; }
+
+    public virtual ICollection<DonHang> DonHangs { get; set; } = new List<DonHang>();
+}
